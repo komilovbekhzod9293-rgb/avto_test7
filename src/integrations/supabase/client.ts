eb98cd2ed6453039d8178b01d40d9af941724458
komilovbekhzod9293-rgb <komilovbekhzod9293@gmail.com>
2026-04-29 03:48:35 +0500

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://ziqzprosgzevkdfwyotl.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppcXpwcm9zZ3pldmtkZnd5b3RsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzNDAwMzAsImV4cCI6MjA4MTkxNjAzMH0.3-4COwffhK2ZU0kU-bnlCWPytsEzRxpMu3SkGg8m7BU";
+const SUPABASE_URL = "https://jvqnrzzkocslgwuminwf.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2cW5yenprb2NzbGd3dW1pbndmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzNDIzNDUsImV4cCI6MjA4MTkxODM0NX0.GEhZndZGdCM5JTQLCBVJ54XkreDnLqpUZk4RBsWjtbk";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
